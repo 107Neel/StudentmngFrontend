8eb395d8589679e17adbafedfda4e95f93e6f464
107Neel <20IT071@charusat.edu.in>
2022-09-13 18:44:24 +0530

    Implement add student in studentmng project

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import Footer from './components/Footer';
 import NavigationBar from './components/NavigationBar';
+import Student from './components/Student';
 
 function App() {
   return (
     <div className="App">
       <NavigationBar/>
       <Footer/>
+      <Student/>
     </div>
   );
 }
