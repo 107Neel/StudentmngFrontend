bfa4b116685b3af7ea3fd00ea921d65b026b0c88
107Neel <20IT071@charusat.edu.in>
2022-09-19 12:47:34 +0530

    Adding Login and Register Links to NavBar

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -3,6 +3,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import {Link} from 'react-router-dom'
+import { faSignInAlt, faUserPlus } from '@fortawesome/free-solid-svg-icons';
 
 export default function NavigationBar() {
   return (
@@ -13,8 +14,12 @@ export default function NavigationBar() {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Link to="/student" className="nav-link">Add Student</Link>
-            <Link to="/listStudents" className="nav-link">View Students</Link>
+          <Link to="student" className="nav-link">Add Student</Link>
+            <Link to="listStudents" className="nav-link">View Students</Link>
+          </Nav>
+          <Nav>
+            <Link to="login" className="nav-link"><FontAwesomeIcon icon={faSignInAlt}/> Login</Link>
+            <Link to="register" className="nav-link"><FontAwesomeIcon icon={faUserPlus}/> Register</Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
